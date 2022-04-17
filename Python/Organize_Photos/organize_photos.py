55fb8ecbe4c7d8841c81e7c804f38f692ff24c07
Gabriella Gervasio <ggervasio117@gmail.com>
2022-04-17 18:25:17 -0400

    refact: Added comments and re-ordered

--- Python/Organize_Photos/organize_photos.py
+++ Python/Organize_Photos/organize_photos.py
@@ -4,20 +4,22 @@ import os
 def extract_place(filename):
     return filename.split("_")[1]
 
-# Make directories based on place name
-def make_place_directories(places):
-    for place in places:
-        os.mkdir(place)
-
 def organize_photos(directory):
     os.chdir(directory)
     # Filenames
     originals = os.listdir()
     places = []
-    # Extract_Place function to extract names in directory 
+    # Call extract_place function to extract each place name 
     for filename in originals:
         place = extract_place(filename)
+        # Append place name to places variable
         places.append(place)
+
+# Make directories based on places variable 
+def make_place_directories(places):
+    # Use places variable to make directories
+    for place in places:
+        os.mkdir(place)
 
     make_place_directories(places)
 
@@ -26,7 +28,3 @@ def organize_photos(directory):
         place = extract_place(filename)
         os.rename(filename, os.path.join(place, filename))
 
-
-# organize_photos("Photos")
-
-print("TEST!")
