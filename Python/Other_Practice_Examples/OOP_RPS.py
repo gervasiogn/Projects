72df61dc01457ac9096f879c7f87db634d2afd09
Gabriella Gervasio <ggervasio117@gmail.com>
2022-07-18 21:25:07 -0400

    Refactored rock, paper, scissors

--- Python/Other_Practice_Examples/OOP_RPS.py
+++ Python/Other_Practice_Examples/OOP_RPS.py
@@ -1,39 +1,23 @@
 import random
 import string
-import re
+
+num_of_rounds = int(input("Please enter the number of rounds you wish to play: "))
 
 class Player:
     def __init__(self, score):
-        self.score = 0
+        self.score = score
         self.moves = ["rock", "paper", "scissors"]
-    
+
     def move(self):
         move1 = input("rock, paper, or scissors? ")
         if move1 in self.moves:
             return move1
         else:
             print("Please pick one of the options. ")
-            
 
     # def learn(self, move1, move2):
     #     self.move1 = self.p1.move
     #     self.move2 = self.p2.move
-
-    # def beats(self, one, two):
-    #     one = self.move1
-    #     two = self.move2
-    #     if ((one == "rock") and (two == "scissors") or 
-    #         (one == "scissors") and (two == "paper") or 
-    #         (one == "paper") and (two == "rock")):
-    #         print("**PLAYER ONE WINS**")
-    #         self.p1.score += 1
-    #     elif (one == two):
-    #         print("IT'S A TIE!")
-    #         num_of_rounds += 1
-    #     else:
-    #         print("**PLAYER TWO WINS**")
-    #         self.p2.score += 1
-    #     print(f"Player 1: {self.p1.score} : Player 2: {self.p2.score}")
 
 class Rock_Player(Player):
     def move(self):
@@ -44,7 +28,7 @@ class Loop_Player(Player):
         index = 0
         for move in self.moves:
             return move[index]
-        index += 1
+            index += 1
 
 class Random_Player(Player):
     def move(self):
@@ -65,7 +49,10 @@ class Game:
         self.p2 = p2
 
     def score(self):
-        if (self.p1.score > self.p2.score):
+        p1_score = self.p1.score()
+        p2_score = self.p2.score()
+        print(f"Player 1 Final Score: {p1_score} Player 2 Final Score: {p2_score}")
+        if (p1_score > p2_score):
             print("PLAYER ONE IS THE CHAMPION")
         else:
             print("PLAYER TWO IS THE CHAMPION")
@@ -75,35 +62,38 @@ class Game:
         move2 = self.p2.move()
         if (move1 == move2):
             print("**IT'S A TIE**")
-            num_of_rounds += 1
-        elif ((move1 == "rock") and (move2 == "scissors") or 
+        elif ((move1 == "rock") and (move2 == "scissors") or
             (move1 == "scissors") and (move2 == "paper") or
             (move1 == "paper") and (move2 == "rock")):
             print("**PLAYER ONE WINS**")
+            self.p1.score += 1
+            print(self.p1.score)
         else:
             print("**PLAYER TWO WINS**")
+            self.p2.score += 1
+            print(self.p2.score)
 
     def play_round(self):
         move1 = self.p1.move()
         move2 = self.p2.move()
         print(f"Player 1: {move1}  Player 2: {move2}")
-        self.beats()
-        
         # self.p1.learn(move1, move2)
         # self.p2.learn(move2, move1)
 
     def play_game(self):
-        while True:
-            try: 
-                num_of_rounds = int(input("Please enter the number of rounds you wish to play: "))
-                print("Game start!")
-                for round in range(num_of_rounds):
-                    print(f"Round {round + 1}:")
-                    self.play_round()
-                self.score()
-                print("Game over!")
-            except (ValueError, TypeError):
-                print("That's not a valid number.")
+        # while True:
+        try:
+            print("Game start!")
+            for round in range(num_of_rounds):
+                print(f"Round {round + 1}:")
+                self.play_round()
+                self.beats()
+                if (f"{self.p1.move()} == {self.p2.move()}"):
+                    num_of_rounds + 1
+            self.score()
+            print("Game over!")
+        except (ValueError, TypeError):
+            print("That's not a valid number.")
 
 if __name__ == '__main__':
     game = Game(Player(0), Rock_Player(0))
